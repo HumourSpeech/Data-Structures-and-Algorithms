3d697c10c09859cde25ca92ea91e47a45104a654
HumourSpeech <mishranitin6076@gmail.com>
2025-07-11 13:58:41 +0530

    Partition Equal subset sum

--- DP/Subsequence2.cpp
+++ DP/Subsequence2.cpp
@@ -1,3 +1,7 @@
+// LEETCODE: 416. Partition Equal Subset Sum
+// Given an integer array nums, return true if you can partition the array into two subsets
+// such that the sum of the elements in both subsets is equal or false otherwise.
+
 #include <bits/stdc++.h>
 using namespace std;
 
