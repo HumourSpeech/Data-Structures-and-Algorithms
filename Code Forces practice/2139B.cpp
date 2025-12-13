5c2155db154a0d5ac3937bab85b158e25242acd1
HumourSpeech <mishranitin6076@gmail.com>
2025-12-13 12:31:44 +0530

    Round 1048 Div2

---
+++ Code Forces practice/2139B.cpp
@@ -0,0 +1,30 @@
+// contest: Codeforces Round 1048 (Div. 2)
+// problem: (B) Cake Collection, Accepted
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+void solve(){
+    int n,t;
+    cin >> n >> t;
+    ll ans = 0;
+    
+    vector <int> a(n);
+    for(int i = 0;i < n;i ++) cin >> a[i];
+    
+    sort(a.begin(), a.end(), greater <int>());
+    for(int i = 0;i < n;i ++) ans +=1ll * a[i] * max(0,t - i);
+    
+    cout << ans << '\n';
+}
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    int TC;
+    cin >> TC;
+    while(TC --){
+        solve();
+    }
+    return 0;
+}
